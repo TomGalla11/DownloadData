616b0f20e25f4bdcb35d2e5336dc1de0210fe5a5
TomGalla11 <67208362+TomGalla11@users.noreply.github.com>
2020-07-22 13:51:44 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,15 @@
+pip install pipenv
+pipenv install opencv-contrib-python
+pipenv install Flask
+pipenv install gunicorn
+pipenv install numpy
+pipenv install jsonpickle
+ls
+echo "# Gfile" >> README.md
+git init
+git add .
+git config --global user.email "tgalla111@gmail.com"
+git config --global user.name "TomGalla11"
+git commit -m "first commit"
+git remote add origin git@github.com:TomGalla11/Gfile.git
+git push -u origin master
